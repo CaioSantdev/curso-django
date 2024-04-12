d409455310b867ad1745b6ff69cd6958aee52fa6
CaioSantDev <c.santosggwp@gmail.com>
2024-04-12 11:50:34 -0300

    ignore

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import path
+
+# Create your views here.
+
+def home(request):
+    return render(request,'recipes/home.html')
+
+
+
+
