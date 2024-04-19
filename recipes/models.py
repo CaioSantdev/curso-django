510e40acec9b499eb6a17ca20e59330adefca3ab
Caio Santos <c.santosggwp@gmail.com>
2024-04-19 11:37:34 -0300

    mudar os dados ficticios

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -15,6 +21,12 @@ class Recipe(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return  self.title
+
+
 
