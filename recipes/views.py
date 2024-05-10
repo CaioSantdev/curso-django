f950f50e378dfbe793dc416e135ac1ee82b62bf3
Caio Santos <c.santosggwp@gmail.com>
2024-05-10 11:59:11 -0300

    sla

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.urls import path
+from django.shortcuts import render,get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 # Create your views here.
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.all().filter(is_published=True)
     return render(request,'recipes/pages/home.html',context={'recipes':recipes,})
 
 def category(request,category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request,'recipes/pages/home.html',context={'recipes':recipes,})
+    # recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    
+    recipes = get_list_or_404(Recipe,category__id=category_id,is_published=True)
+    return render(request,'recipes/pages/home.html',context={'recipes':recipes,
+                                                             'title':f'{recipes[0].category.name} - Category | '})
 
 def recipe(request,id):
     return render(request,'recipes/pages/recipe-view.html',context={
